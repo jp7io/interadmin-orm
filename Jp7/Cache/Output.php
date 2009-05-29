e047618e2e3f6f59b2ae4c0abdab262986ebf3dc
carlos <carlos@jp7.com.br>
2009-05-29 16:13:09 +0000

    - CHG: Adicionada verificação do log dentro da pasta do InterAdmin.

--- Jp7/Cache/Output.php
+++ Jp7/Cache/Output.php
@@ -11,6 +11,9 @@
  {
 	 protected static $_instance = null;
 	 protected static $_started = false;
+	 protected static $_enabled = false;
+	 protected static $_cachedir = './cache/';
+	 protected static $_logdir = './interadmin/';
 	 
 	 /**
 	  * Retorna uma instância configurada do Jp7_Cache_Page.
@@ -26,24 +29,31 @@
 			global $debugger;
 			$config = Zend_Registry::get('config');
 			
-			$cacheEnabled = ($config->cache && !$debugger->debugFilename && !$debugger->debugSql);
-	
-			// Front
+			if ($config->cache && !$debugger->debugFilename && !$debugger->debugSql) {
+				self::$_enabled = true;
+			}
+			
 			$frontDefault = array(
 				'lifetime' => 86400 // 1 dia
 			);
-
-			$frontend = new Jp7_Cache_Output($frontOptions + $frontDefault);
-			
-			// Back
 			$backDefault = array(
-				'cache_dir' => './cache/'
+				'cache_dir' => self::$_cachedir,
+				'file_name_prefix' => 'zf'
 			);
-	
-			$backend = new Zend_Cache_Backend_File($backOptions + $backDefault);
-			 
-			$frontend->setBackend($backend);
-			 
+
+			self::$_cachedir = $backDefault['cache_dir'];
+			
+			$frontend = new Jp7_Cache_Output($frontOptions + $frontDefault);
+
+			if (is_dir(self::$_cachedir)) {
+				$backend = new Zend_Cache_Backend_File($backOptions + $backDefault);
+			} else {
+				$backend = new Zend_Cache_Backend_Test();
+				self::$_enabled = false;
+			}
+
+			$frontend->setBackend($backend);		
+						 
 			self::$_instance = $frontend;
 		}
 		return self::$_instance;
@@ -57,33 +67,29 @@
 	  */	 
 	 public function start()
 	 {
-	 	// Criando ID pelo module/controller/action
-	 	$params = Zend_Controller_Front::getInstance()->getRequest()->getParams();
+	 	global $c_jp7;
 
-	 	$id = toId(implode('_', array(
-	 		$params['controller'],
-	 		$params['action'], 
-	 		$params['lang'], 
-	 		$params['module'])
-	 	));
-
-	 	if (func_num_args()) {
-	 		$args = func_get_args();
-	 		if (count($args) == 1 && is_string($args[0])) {
-	 			$id .= '_' . toId($args[0]);
-	 		} else {
-	 			$id .= '_' . md5(serialize(func_get_args()));
-	 		}
+	 	if (!self::$_enabled) {
+	 		return false;
 	 	}
+
+	 	// Gera o id do cache
+	 	$id = $this->_makeId(func_get_args());
+
+	 	// Verifica se o log foi alterado
+	 	$this->_checkLog();
 
 	 	$retorno = parent::start($id);
-		
+
 	 	if ($retorno) {
+	 		if ($c_jp7) {
+	 			echo '<div style="position:absolute;left:0px;top:0px;padding:5px;background:#FFCC00;filter:alpha(opacity=50);opacity: .5;z-index:1000">CACHE</div>';
+	 		} 
 	 		exit;
 	 	}
-		
+
 	 	self::$_started = true;
-		
+
 	 	return $retorno;
 	 }
 
@@ -96,4 +102,53 @@
 	 {
 	 	return self::$_started;
 	 }
+	 
+	 
+	 /**
+	  * Cria um ID na forma: controller_action_lang_module_SUFIXO
+	  * 
+	  * @param mixed $data Gera um hash e adiciona como sufixo ao ID.
+	  * @return string ID gerado.
+	  */
+	 protected function _makeId($data)
+	 {
+	 	$params = Zend_Controller_Front::getInstance()->getRequest()->getParams();
+
+	 	$id = toId(implode('_', array(
+	 		$params['controller'],
+	 		$params['action'], 
+	 		$params['lang'], 
+	 		$params['module'])
+	 	));
+
+	 	if ($data) {
+			if (count($data) == 1 && is_string($data[0])) {
+		 		$id .= '_' . toId($data[0]);
+		 	} else {
+		 		$id .= '_' . md5(serialize($data));
+		 	}
+	 	}
+
+	 	return $id;
+	}
+
+	/**
+	 * Verifica se o log do InterAdmin foi alterado. E limpa o cache se necessário.
+	 * 
+	 * @return void
+	 */
+	protected function _checkLog()
+	{
+		$lastLogFilename = 'logcheck.log';
+		$lastLogTime = intval(file_get_contents(self::$_cachedir . $lastLogFilename));
+
+		// Verificação do log
+	 	$logTime = filemtime(self::$_logdir . 'interadmin.log');
+		
+	 	// Grava último log, se necessário
+	 	if ($logTime != $lastLogTime) {
+	 		$this->clean();
+	 		file_put_contents(self::$_cachedir . $lastLogFilename, $logTime);
+	 	}
+	}
 }
