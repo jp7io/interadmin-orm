83d08d8944a6da7c6322b5c879e94fbeec0f6291
marcos <marcos@jp7.com.br>
2010-02-23 19:19:09 +0000

    - BUG: Inserido if para compatibilidade quando id_tipo é um objeto.

--- InterAdminTipo.class.php
+++ InterAdminTipo.class.php
@@ -51,7 +51,10 @@ class InterAdminTipo extends InterAdminAbstract {
 	 * @param array $options Default array of options. Available keys: db_prefix, fields.
 	 */
 	public function __construct($id_tipo = 0, $options = array()) {
-		$this->id_tipo = $id_tipo;
+		if (is_object($id_tipo)) {
+			$id_tipo = (string) $id_tipo;
+		}
+		$this->id_tipo = intval($id_tipo);
 		$this->db_prefix = ($options['db_prefix']) ? $options['db_prefix'] : $GLOBALS['db_prefix'];
 		if ($options['fields']) {
 			$this->getFieldsValues($options['fields']);
