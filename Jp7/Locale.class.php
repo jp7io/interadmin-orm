0db8ed4a9d58a187f4c0b287c3df5ed2470c9b7b
carlos <carlos@jp7.com.br>
2010-06-16 14:32:22 +0000

    - CHG: Adicionados comentários e adicionado método __toString().

--- Jp7/Locale.class.php
+++ Jp7/Locale.class.php
@@ -8,10 +8,32 @@
  */
 class Jp7_Locale
 {
+	/**
+	 * Identificador do idioma, exemplo: 'en', 'pt-br', 'de', etc.
+	 * @var string 
+	 */
 	public $lang = '';
+	/**
+	 * String que é adicionada ao prefixo das tabelas no banco de dados. Fica vazio na língua padrão.
+	 * @var string  
+	 */
 	public $prefix = '';
+	/**
+	 * Path a ser adicionado à URL do site para se abrir com esse idioma. Fica vazio na língua padrão.
+	 * @var string  
+	 */
 	public $path = '';
-
+	
+	public function __toString() {
+		return $this->lang;
+	}
+	
+	/**
+	 * Construtor público.
+	 * 
+	 * @param string $language 	pt-br, en, etc...
+	 * @return 
+	 */
 	public function __construct($language)
 	{
 		$config = Zend_Registry::get('config');
