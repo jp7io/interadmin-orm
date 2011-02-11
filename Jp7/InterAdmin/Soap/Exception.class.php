6377130be16b6a74b67d62f8d098d43f96e38854
carlos <carlos@jp7.com.br>
2011-02-11 14:45:12 +0000

    - CHG: Adicionado UTF8 encode à Jp7_InterAdmin_Soap_Exception.

--- Jp7/InterAdmin/Soap/Exception.class.php
+++ Jp7/InterAdmin/Soap/Exception.class.php
@@ -6,5 +6,8 @@
  */
 class Jp7_InterAdmin_Soap_Exception extends Exception {
 	
+	public function __construct ($message = "", $code = 0) {
+		return parent::__construct(utf8_encode($message), $code);
+	}
 	
 }
