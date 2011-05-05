f5b132bec518fb36256b9eede12e83033051407c
carlos <carlos@jp7.com.br>
2011-05-05 23:00:14 +0000

    - NEW: Adicionando modelo Content.

---
+++ Jp7/View/Helper/ItemClass.php
@@ -0,0 +1,25 @@
+<?php
+
+class Jp7_View_Helper_ItemClass extends Zend_View_Helper_Abstract {
+	
+	public function ItemClass($count, $key, $item = null) {
+		$classes = array();
+		if ($key == 0) {
+			$classes[] = 'first';
+		}
+		if ($key + 1 == $count) {
+			$classes[] = 'last';
+		}
+		if ($key % 2) {
+			$classes[] = 'even';
+		} else {
+			$classes[] = 'odd';
+		}
+		if ($item instanceof InterAdmin) {
+			$classes[] = 'id-' . $item->id;
+			$classes[] = 'tipo-' . $item->id_tipo;
+		}
+		//$classes[] = 'pos-' . ($key + 1);
+		return implode(' ', $classes);
+	}
+}
--- Jp7/View/Helper/Link.php
+++ Jp7/View/Helper/Link.php
@@ -2,12 +2,7 @@
 
 class Jp7_View_Helper_Link extends Zend_View_Helper_Abstract {
 	
-	protected $href;
-	protected $text;
-	
 	public function Link($href, $text = '') {
-		$this->href = $href;
-		$this->text = $text;
-		return '<a href="' . $this->href . '">' . $this->text . '</a>';
+		return '<a href="' . $href . '">' . $text . '</a>';
 	}
 }
