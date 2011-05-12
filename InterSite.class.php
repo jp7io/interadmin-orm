aee7b2e5a0071f00d0357340219d9dbf04639a40
carlos <carlos@jp7.com.br>
2011-05-12 19:50:25 +0000

    - NEW: Adicionada exceção para o InterAdmin remote no IP 189.126.109.83.

--- InterSite.class.php
+++ InterSite.class.php
@@ -180,6 +180,10 @@ class InterSite {
 		
 		if ($this->server) {
 			$this->db = clone $this->server->db;
+			// Exceção para funcionamento do InterAdmin Remote nos sites Express
+			if ($this->db->host == '189.126.109.83' && $this->hostType == self::HOST_REMOTE) {
+				$this->db->host = 'localhost';
+			}
 			if ($this->db->host_internal && $this->hostType != self::HOST_REMOTE) {
 				$this->db->host = $this->db->host_internal;
 			}
