86cd36cf228c023101bdbb367ece2c805402862e
carlos <carlos@jp7.com.br>
2011-06-01 19:11:53 +0000

    - NEW: Adicionado modelo para Configurações Gerais do Site.

--- Jp7/Controller/Action.class.php
+++ Jp7/Controller/Action.class.php
@@ -46,7 +46,7 @@ class Jp7_Controller_Action extends Zend_Controller_Action
 				$template_data = unserialize($siteSettings->template_data);
 				if ($config->template_path = $template_data['css_template']) {
 					$this->view->headLink()->removeStylesheet('css/main.css');
-					$this->view->headLink()->appendStylesheet($config->template_path . '/css/main.css');
+					$this->view->headLink()->appendStylesheet($config->template_path . '/css/main.css?clientePath=' . $config->name_id);
 				}
 			}
 		}
