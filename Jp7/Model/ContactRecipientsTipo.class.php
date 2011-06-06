107e26e21644fc89e1c55511591f8aae4fc55b86
carlos <carlos@jp7.com.br>
2011-06-06 22:06:31 +0000

    - CHG: Correções e Ajustes gerais versão 0.2.  Tipos de destinatários não está com order by por ordem, nome. Issue: EXPRESS-47

--- Jp7/Model/ContactRecipientsTipo.class.php
+++ Jp7/Model/ContactRecipientsTipo.class.php
@@ -7,7 +7,7 @@ class Jp7_Model_ContactRecipientsTipo extends Jp7_Model_TipoAbstract {
 	public $attributes = array(
 		'id_tipo' => 'ContactRecipients',
 		'nome' => 'Contato - Destinatários',
-		'campos' => 'varchar_key{,}Nome{,}{,}{,}{,}{,}0{,}{,}{,}{,}{,}{,}{,}{,}{,}name{;}varchar_1{,}E-mail{,}{,}{,}S{,}S{,}email{,}S{,}{,}{,}{,}{,}{,}{,}{,}email{;}int_key{,}Ordem{,}{,}{,}{,}{,}0{,}{,}1{,}{,}{,}{,}{,}{,}{,}ordem{;}char_key{,}Mostrar{,}{,}{,}{,}{,}0{,}{,}{,}{,}{,}{,}{,}{,}{,}mostrar{;}',
+		'campos' => 'varchar_key{,}Nome{,}{,}{,}{,}{,}0{,}{,}2{,}{,}{,}{,}{,}{,}{,}name{;}varchar_1{,}E-mail{,}{,}{,}S{,}S{,}email{,}S{,}{,}{,}{,}{,}{,}{,}{,}email{;}int_key{,}Ordem{,}{,}{,}{,}{,}0{,}{,}1{,}{,}{,}{,}{,}{,}{,}ordem{;}char_key{,}Mostrar{,}{,}{,}{,}{,}0{,}{,}{,}{,}{,}{,}{,}{,}{,}mostrar{;}',
 		'children' => '',
 		'arquivos_ajuda' => '',
 		'arquivos' => '',
