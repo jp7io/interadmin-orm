6dc33b768a8bbd05737097398b5de156eea029ce
carlos <carlos@jp7.com.br>
2011-06-17 19:01:20 +0000

    - NEW: Adicionando icone para arquivos .xlsx

--- Jp7/View/Helper/FileIcon.php
+++ Jp7/View/Helper/FileIcon.php
@@ -3,7 +3,7 @@
 class Jp7_View_Helper_FileIcon extends Zend_View_Helper_Abstract {
 	
 	public function FileIcon($file) {
-		$availableIcons = array('doc', 'docx', 'flv', 'gif', 'jpg', 'mp3', 'pdf', 'png', 'pps', 'ppt', 'wan', 'wfl', 'wmv', 'wtl', 'xls', 'zip');
+		$availableIcons = array('doc', 'docx', 'flv', 'gif', 'jpg', 'mp3', 'pdf', 'png', 'pps', 'ppt', 'wan', 'wfl', 'wmv', 'wtl', 'xls', 'zip', 'xlsx');
 		$ext = $file->getExtension();
 		ob_start();
 		if (in_array($ext, $availableIcons)) { 
