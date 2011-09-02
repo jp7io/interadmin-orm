e18d8edec2913f7e3d45ebf39fc83f93f2dea00e
carlos <carlos@jp7.com.br>
2011-09-02 21:02:44 +0000

    - NEW: JSTree.class.php em desenvolvimento

--- Jp7/InterAdmin/JSTree.class.php
+++ Jp7/InterAdmin/JSTree.class.php
@@ -5,15 +5,23 @@ class Jp7_InterAdmin_JSTree {
 	public $tipos = array();
 	
 	public function __construct($options = array()) {
-		$options = InterAdmin::mergeOptions(array(
-			'fields' => array('nome', 'parent_id_tipo', 'model_id_tipo', 'admin'),
-			'use_published_filters' => true,
-			'class' => 'InterAdminTipo'	
-		), $options);
+		global $lang;
 		
-		$all = InterAdminTipo::findTipos($options);
-		
-		$this->tipos = self::groupByParent($all);
+		if (!$options['static']) {
+			$options = InterAdmin::mergeOptions(array(
+				'fields' => array('nome', 'parent_id_tipo', 'model_id_tipo', 'admin'),
+				'use_published_filters' => true,
+				'class' => 'InterAdminTipo'	
+			), $options);
+			
+			if ($lang->prefix) {
+				$options['fields'][] = 'nome' . $lang->prefix; 
+			}
+			
+			$all = InterAdminTipo::findTipos($options);
+									
+			$this->tipos = self::groupByParent($all);
+		}
 	}
 	
 	public static function groupByParent($all) {
@@ -25,11 +33,12 @@ class Jp7_InterAdmin_JSTree {
 	}
 	
 	public function addTipo(&$tree, $tipo, $nivel = 0) {
-		global $tipos;
+		global $tipos, $lang;
 		
+		$nome_lang = ($lang->prefix && $tipo->{'nome' . $lang->prefix}) ? $tipo->{'nome' . $lang->prefix} : $tipo->nome;
 		$node = (object) array(
-			'data' => utf8_encode($tipo->nome),
-			'attr' => array(),
+			'data' => utf8_encode($nome_lang),
+			//'attr' => array(),
 			'metadata' => array(
 				'id_tipo' => $tipo->id_tipo,
 				'model_id_tipo' => $tipo->model_id_tipo
@@ -58,13 +67,32 @@ class Jp7_InterAdmin_JSTree {
 	}
 	
 	public function createTree() {
-		foreach ($this->tipos[0] as $tipo) {
-			$this->addTipo($this->tree, $tipo);
+		if (!$this->tree) {
+			if ($this->tipos[0]) {
+				foreach ($this->tipos[0] as $tipo) {
+					$this->addTipo($this->tree, $tipo);
+				}
+			}
 		}
 		return $this->tree;
 	}
 	
 	public function toJson(){
 		return json_encode($this->createTree());	
+	}
+	
+	public function addNode($nome, $callback = array()) {
+		$node = $this->createNode($nome, $callback);
+		$this->tree[] = $node;
+		return $node;
+	}
+	
+	public function createNode($nome, $callback = array()) {
+		return (object) array(
+			'data' => Jp7_Utf8::encode($nome),
+			'metadata' => array(
+				'callback' => utf8_encode($callback)
+			)
+		);
 	}
 }
