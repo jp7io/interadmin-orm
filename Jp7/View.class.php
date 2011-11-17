8a724d383c5242b2f3e0b03de3e59a90c9820e62
marcos <marcos@jp7.com.br>
2011-11-17 17:24:06 +0000

    - CHG: Inserindo XHTML e encoding.

--- Jp7/View.class.php
+++ Jp7/View.class.php
@@ -4,6 +4,8 @@ class Jp7_View extends Zend_View {
 	public function __construct($config = array())
     {
     	parent::__construct($config);
+		$this->doctype('XHTML1_STRICT');
+		$this->setEncoding('ISO-8859-1');
 		$this->addHelperPath('Jp7/View/Helper', 'Jp7_View_Helper');
     }	
 }
