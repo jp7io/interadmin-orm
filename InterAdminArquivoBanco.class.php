1c60999a8e3f1d11017be42d557dbdb90fc6275f
carlos <carlos@jp7.com.br>
2012-09-17 14:43:04 +0000

    - BUG: Tabela estava errada na nova classe InterAdminArquivoBanco.

--- InterAdminArquivoBanco.class.php
+++ InterAdminArquivoBanco.class.php
@@ -13,7 +13,7 @@ class InterAdminArquivoBanco {
 	 * @return string  id_arquivo_banco
 	 */
 	public function addFile($fieldsValues) {
-		$id_arquivo_banco = jp7_db_insert($this->getTableName() . '_banco', 'id_arquivo_banco', '', $fieldsValues);
+		$id_arquivo_banco = jp7_db_insert($this->getTableName(), 'id_arquivo_banco', '', $fieldsValues);
 		return str_pad($id_arquivo_banco, 8, '0', STR_PAD_LEFT);
 	}
 	
