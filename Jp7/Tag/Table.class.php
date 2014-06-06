ac0e938b5f1f968a1dd8151c6f766b64c94ed0e6
carlos <carlos@jp7.com.br>
2014-06-06 17:17:13 +0000

    - CHG: Usando Jp7_Tag para as tabelas.

--- Jp7/Tag/Table.class.php
+++ Jp7/Tag/Table.class.php
@@ -7,4 +7,20 @@ class Jp7_Tag_Table extends Jp7_Tag_Container {
 		$this->add($tr);
 		return $tr;
 	}
+	
+	public function rotate() {
+		$trs = $this->getItems();
+		
+		$out = array();
+		foreach ($trs as $col_i => $row) {
+			foreach ($row->getItems() as $row_i => $cell) {
+				if (!$out[$row_i]) {
+					$out[$row_i] = new Jp7_Tag_Tr();
+				}
+				$out[$row_i]->add($cell);
+			}
+		}
+		
+		$this->setItems($out);
+	}
 }
