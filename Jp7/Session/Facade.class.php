a990115fa64536dc45deb43d228e645e2f770dd8
carlos <carlos@jp7.com.br>
2014-08-04 13:40:35 +0000

    - CHG: Adicionada classe que faz com que sessão não crie lock. (Sessões em arquivo criam lock que em sites com frames ou AJAX concorrentes, pode travar um dos requests enquanto o outro não termina.)

---
+++ Jp7/Session/Facade.class.php
@@ -0,0 +1,33 @@
+<?php
+
+class Jp7_Session_Facade implements ArrayAccess {
+	private $_data = array();
+	public function __construct($session) {
+		$this->_data = $session;
+		session_write_close();
+	}
+	public function offsetSet($offset, $value) {
+		if (is_null($offset)) {
+			$this->_data[] = $value;
+		} else {
+			$this->_data[$offset] = $value;
+		}
+		$this->syncData();
+	}
+	public function offsetExists($offset) {
+		return isset($this->_data[$offset]);
+	}
+	public function offsetUnset($offset) {
+		unset($this->_data[$offset]);
+		$this->syncData();
+	}
+	public function offsetGet($offset) {
+		return isset($this->_data[$offset]) ? $this->_data[$offset] : null;
+	}
+	private function syncData() {
+		session_start();
+		$_SESSION = $this->_data;
+		session_write_close();
+		$_SESSION = $this;
+	}
+}
