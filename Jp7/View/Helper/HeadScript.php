0037151bc6b420791b6fabeb49351965a2f3e530
allan <allan@jp7.com.br>
2014-09-04 18:35:21 +0000

    - NEW: Ao inserir um script file é possível utilizar o parâmetro "async" via argumentos.

--- Jp7/View/Helper/HeadScript.php
+++ Jp7/View/Helper/HeadScript.php
@@ -40,4 +40,56 @@ class Jp7_View_Helper_HeadScript extends Zend_View_Helper_HeadScript {
 		}
 		return parent::toString($indent);	
 	}
+
+	/**
+     * Create script HTML
+     *
+     * @param  string $type
+     * @param  array $attributes
+     * @param  string $content
+     * @param  string|int $indent
+     * @return string
+     */
+    public function itemToString($item, $indent, $escapeStart, $escapeEnd)
+    {
+        $attrString = '';
+        if (!empty($item->attributes)) {
+            foreach ($item->attributes as $key => $value) {
+                if (!$this->arbitraryAttributesAllowed()
+                    && !in_array($key, $this->_optionalAttributes))
+                {
+                    continue;
+                }
+
+                if ($key == 0 && $value == 'async') {
+                	$attrString .= sprintf(' %s', $value);
+                	continue;
+                }
+
+                if ('defer' == $key) {
+                    $value = 'defer';
+                }
+
+                $attrString .= sprintf(' %s="%s"', $key, ($this->_autoEscape) ? $this->_escape($value) : $value);
+            }
+        }
+
+        $type = ($this->_autoEscape) ? $this->_escape($item->type) : $item->type;
+        $html  = '<script type="' . $type . '"' . $attrString . '>';
+        if (!empty($item->source)) {
+              $html .= PHP_EOL . $indent . '    ' . $escapeStart . PHP_EOL . $item->source . $indent . '    ' . $escapeEnd . PHP_EOL . $indent;
+        }
+        $html .= '</script>';
+
+        if (isset($item->attributes['conditional'])
+            && !empty($item->attributes['conditional'])
+            && is_string($item->attributes['conditional']))
+        {
+            $html = $indent . '<!--[if ' . $item->attributes['conditional'] . ']> ' . $html . '<![endif]-->';
+        } else {
+            $html = $indent . $html;
+        }
+
+        return $html;
+    }
 }
