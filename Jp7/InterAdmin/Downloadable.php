ad706fcae2d2c56dbe4fa4dec0e5215aaad0ee27
anderson <anderson@jp7.com.br>
2014-12-16 15:11:56 +0000

    - CHG: Implementando getExtension() e getAbsoluteUrl().

--- Jp7/InterAdmin/Downloadable.php
+++ Jp7/InterAdmin/Downloadable.php
@@ -7,34 +7,26 @@ trait Downloadable {
 	public function getUrl() {
 		return str_replace('../../upload', 'assets', $this->url);
 	}
-
+	
 	// Absolute client side URL
 	public function getAbsoluteUrl() {
-		kd('Not implemented');
-		$config = InterSite::config();
-		global $jp7_app;
-		
-		if ($jp7_app && $jp7_app != 'interadmin') {
-			return jp7_replace_beginning('../../upload/', 'http://' . $config->server->host . '/' . $config->name_id . '/' . $jp7_app . '/upload/', $this->url);
-		} else {
-			return jp7_replace_beginning('../../upload/', $config->url . 'upload/', $this->url);
-		}
+		$config = \InterSite::config();
+		return $config->url. $this->getUrl();
 	}	
 	
 	// Server side file name
 	public function getFilename() {
-		return str_replace('../../', storage_path() . '/', $this->url);
+		$path = parse_url($this->url)['path']; // remove query string
+		return str_replace('../../', storage_path() . '/', $path);
 	}
-
+	
 	/**
 	 * Returns the extension of this file.
 	 * 
 	 * @return string Extension, such as 'jpg' or 'gif'.
 	 */
 	public function getExtension() {
-		kd('Not implemented');
-		$url = reset(explode('?', $this->url));
-		return preg_replace('/(.*)\.(.*)$/', '\2', $url);
+		return pathinfo($this->getFilename(), PATHINFO_EXTENSION);
 	}
 
 	public function getSize() {
