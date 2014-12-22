b75aa6d1d5db1ceb504269bf759f4881ee2132cb
jp7carlos <carlos@jp7.com.br>
2014-12-22 13:19:44 -0200

    Add cache to dynamic classes

--- Jp7/InterAdmin/DynamicLoader.php
+++ Jp7/InterAdmin/DynamicLoader.php
@@ -3,24 +3,58 @@
 namespace Jp7\InterAdmin;
 
 class DynamicLoader {
-	
+	private static $classMap = [];
+	private static $classTipoMap = [];
+
 	// Cria classes cadastradas no InterAdmin sem a necessidade de criar um arquivo para isso
 	public static function load($class) {
-		$tipo = \InterAdminTipo::findFirstTipo(array(
-			'where' => array(
-				"(class = '" . addslashes($class) . "' OR class_tipo = '" . addslashes($class) . "')"
-			)
-		));
-		if ($tipo) {
-			if ($tipo->class === $class) {
-				$code = \Jp7_InterAdmin_Util::gerarClasseInterAdmin($tipo, false);
-			} else {
-				$code = \Jp7_InterAdmin_Util::gerarClasseInterAdminTipo($tipo, false);
-			}
+		if (empty(self::$classMap)) {
+			self::createClassMap();
+		}
+
+		$code = null;
+		if ($id_tipo = self::$classMap[$class]) {
+			$tipo = new \InterAdminTipo($id_tipo);
+			$code = \Jp7_InterAdmin_Util::gerarClasseInterAdmin($tipo, false);
+		} elseif ($id_tipo = self::$classTipoMap[$class]) {
+			$tipo = new \InterAdminTipo($id_tipo);
+			$code = \Jp7_InterAdmin_Util::gerarClasseInterAdminTipo($tipo, false);
+		}
+		if ($code) {
 			eval('?>' . $code);
 			return true;
 		}
 		
 		return false;
+	}
+
+	public static function createClassMap() {
+		$maps = \Cache::remember('Interadmin.classmap', 60, function() {
+			$classMap = [];
+			$classTipoMap = [];
+
+			$tipos = \InterAdminTipo::findTipos(array(
+				'fields' => array('class', 'class_tipo'),
+				'where' => array(
+					"(class <> '' OR class_tipo <> '')"
+				),
+				'class' => 'stdClass'
+			));
+			
+			foreach ($tipos as $tipo) {
+				$attr = &$tipo->attributes;
+				if ($attr['class']) {
+					$classMap[$attr['class']] = $attr['id_tipo'];
+				}
+				if ($attr['class_tipo']) {
+					$classTipoMap[$attr['class_tipo']] = $attr['id_tipo'];
+				}
+			}
+
+			return compact('classMap', 'classTipoMap');
+		});
+
+		self::$classMap = $maps['classMap'];
+		self::$classTipoMap = $maps['classTipoMap'];
 	}
 }
