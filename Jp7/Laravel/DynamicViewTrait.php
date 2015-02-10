ae252b2c3f4b4c378735773ebfb43c1545c6c1cc
Carlos Rodrigues <carlos@jp7.com.br>
2015-02-10 19:33:34 -0200

    Separate controller in traits

---
+++ Jp7/Laravel/DynamicViewTrait.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Jp7\Laravel;
+
+trait DynamicViewTrait {
+
+	/**
+	 * @var Variables to send to view
+	 */
+	protected $_viewData = null;
+
+	public function constructDynamicViewTrait() {
+		if (is_null($this->_viewData)) {
+			$this->_viewData = new \stdClass();
+		}
+	}
+	
+	public function &__get($key) {
+		return $this->_viewData->$key;
+	}
+	
+	public function __set($key, $value) {
+		$this->_viewData->$key = $value;
+	}
+
+	/**
+	 * Execute an action on the controller.
+	 *
+	 * @param  string  $method
+	 * @param  array   $parameters
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	public function callAction($method, $parameters) {
+		$response = call_user_func_array(array($this, $method), $parameters);
+		
+		if (!is_null($response)) {
+			return $response;
+		}
+		if ($method == 'show' && !$this->record) {
+			throw new \Exception('Show action without record. You need to set $this->record inside your controller.');	
+		}
+		return $this->_makeView($method);
+	}
+	
+	private function _makeView($method) {
+		$viewName = $this->_getViewName($method);
+		$viewContent = \View::make($viewName, (array) $this->_viewData);
+		
+		if ($this->layout) {
+			$this->_viewData->content = $viewContent;
+			$viewContent = \View::make($this->layout, (array) $this->_viewData);
+		}
+		return $viewContent;
+	}
+
+	/**
+	 * Get the view name
+	 * 
+	 * @return string
+	 */
+	private function _getViewName($method) {
+		//\Route::getCurrentRoute()->getAction()['as']
+
+		$action = str_replace('_', '-', snake_case($method));
+		
+		$viewRoute = $this->_viewRoute(get_class($this), $action);
+		if ($this->_viewExists($viewRoute)) {
+			return $viewRoute;
+		}
+		
+		$viewRoute = $this->_viewRoute(get_parent_class($this), $action);
+		if ($this->_viewExists($viewRoute)) {
+			return $viewRoute;
+		}
+		
+		return "templates.{$action}";
+	}
+
+	protected function _viewRoute($controllerClass, $action) {
+		$controllerRoute = $this->_controllerRoute($controllerClass);
+		return "{$controllerRoute}.{$action}";
+		
+	}
+	
+	protected function _viewExists($route) {
+		$filename = str_replace('.', '/', $route);
+		
+		foreach (\Config::get('view.paths') as $viewPath) {
+			if (file_exists($viewPath . '/' . $filename . '.blade.php')) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	protected function _controllerRoute($controllerClass) {
+		$explodedClassName = explode('\\', $controllerClass);
+		$slugArray = array_map(function($string) {
+			$string = snake_case($string);
+			$string = str_replace('_', '-', $string);
+			return str_replace('-controller', '', $string);
+		}, $explodedClassName);
+		return implode('.', $slugArray);
+	}
+}
