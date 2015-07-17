68012325d823b376d9048dfdeab1920452b44131
Carlos Rodrigues <carlos@jp7.com.br>
2015-07-17 11:25:09 -0300

    Add version parameter

--- Jp7/Laravel5/Cdn.php
+++ Jp7/Laravel5/Cdn.php
@@ -4,19 +4,20 @@ namespace Jp7\Laravel5;
 
 class Cdn
 {
-    public static function asset($url)
+    public static function asset($url, $version = false)
     {
-        return self::replace(asset($url)) .'?v='.self::getVersion();
+        return self::replace(asset($url)).
+            ($version ? '?v='.self::getVersion() : '');
     }
 
     public static function css($url)
     {
-        return '<link href="'.self::asset($url).'"  rel="stylesheet" type="text/css">';
+        return '<link href="'.self::asset($url, true).'"  rel="stylesheet" type="text/css">';
     }
 
     public static function js($url)
     {
-        return '<script src="'.self::asset($url).'"></script>';
+        return '<script src="'.self::asset($url, true).'"></script>';
     }
 
     private static function replace($url)
