568fcd057b7dbd332a41904de52ad53b80fe5229
Carlos Rodrigues <carlos@jp7.com.br>
2015-07-17 11:35:57 -0300

    Ask password for qa.* and alt.*

---
+++ Jp7/Laravel5/Middleware/QaAuth.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Jp7\Laravel5\Middleware;
+
+use Closure;
+
+class QaAuth
+{
+    // Asks password for qa.* or alt.*
+    public function handle($request, Closure $next)
+    {
+        $host = $request->getHost();
+        if (starts_with($host, 'qa.') || starts_with($host, 'alt.')) {
+            $name = config('app.name');
+            if (empty($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] != $name || $_SERVER['PHP_AUTH_PW'] != $name) {
+                header('WWW-Authenticate: Basic realm="'.$name.'"');
+                header('HTTP/1.0 401 Unauthorized');
+                echo '401 Unauthorized';
+                exit;
+            }
+        }
+        
+        return $next($request);
+    }
+}
