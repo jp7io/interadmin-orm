2a6d34c6bc12607fcc0dd64b469647a50d74c6b1
Carlos Rodrigues <carlos@jp7.com.br>
2015-07-21 11:20:07 -0300

    Remove exception when there is a form

--- Jp7/HttpCache/HttpCacheExtension.php
+++ Jp7/HttpCache/HttpCacheExtension.php
@@ -10,7 +10,7 @@ class HttpCacheExtension extends HttpCache
 {
     public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
     {
-        if ($this->matchesBlacklist($request)) {
+        if (!config('httpcache.enabled') || $this->matchesBlacklist($request)) {
             return $this->pass($request, $catch);
         }
         
@@ -18,19 +18,7 @@ class HttpCacheExtension extends HttpCache
             return $this->invalidate($request, $catch);
         }
         
-        if (!config('httpcache.enabled')) {
-            $response = $this->pass($request, $catch);
-        } else {
-            $response = parent::handle($request, $type, $catch);
-        }
-        
-        if ($response instanceof \Illuminate\Http\Response) {
-            if (str_contains($response->getContent(), 'method="POST"')) {
-                throw new \Exception('Forms with CSRF token should not be cached. Add it to httpcache.blacklist.');
-            }
-        }
-        
-        return $response;
+        return parent::handle($request, $type, $catch);
     }
     
     public function matchesBlacklist(Request $request)
