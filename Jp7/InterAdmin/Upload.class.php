d6ddbcfdc562ced2a90ea2a3e50bd846343d0de7
Rafael Testa <rafael@jp7.com.br>
2015-08-05 17:44:57 -0300

    Add upload class

---
+++ Jp7/InterAdmin/Upload.class.php
@@ -0,0 +1,65 @@
+<?php
+
+use League\Url\Url;
+
+class Jp7_InterAdmin_Upload
+{
+
+    // Has upload[url] set on config (S3 or Local)
+    public static function hasExternalUpload()
+    {
+        global $config;
+        return $config->upload && $config->upload['url'];
+    }
+
+    public static function uploadUrl($path)
+    {
+        global $config;
+        $url = Url::createFromUrl($config->upload['url']);
+        $url->setScheme('http');
+        $url->setPath($path);
+        return $url;
+    }
+
+    // No upload[url] setting
+    public static function defaultUrl($path)
+    {
+        global $config;
+        $url = Url::createFromUrl($config->server->host);
+        $url->setScheme('http');
+        return $url->setPath($config->name_id . '/' . $path);
+    }
+
+    public static function addImageTemplate($path, $template = 'original')
+    {
+        return str_replace('upload/', 'imagecache/' . $template . '/', $path);
+    }
+
+    /**
+     * Altera o endereço para que aponte para a url do cliente.
+     *
+     * @param $url Url do arquivo.
+     *
+     * @return string
+     */
+    public static function url($path = '../../', $template = 'original')
+    {
+        if (starts_with($path, '../../')) {
+            $path = mb_substr($path, 6);// Remove '../../'
+        }
+
+        $path = self::addImageTemplate($path, $template);
+
+        if (!starts_with($path, 'http://')) {
+            if (self::hasExternalUpload()) {
+                $url = self::uploadUrl($path);
+            } else {
+                $url = self::defaultUrl($path);
+            }
+        } else { // Absolute URL => Wont change
+            $url = Url::createFromUrl($path);
+        }
+
+        return $url->__toString();
+    }
+}
