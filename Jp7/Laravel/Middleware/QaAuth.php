77f4fe79fcf9cb77695bdcbbc199ce840a22682b
Carlos Rodrigues <carlos@jp7.com.br>
2015-08-12 11:20:33 -0300

    QaAuth now uses env instead of hostname

--- Jp7/Laravel/Middleware/QaAuth.php
+++ Jp7/Laravel/Middleware/QaAuth.php
@@ -3,22 +3,20 @@
 namespace Jp7\Laravel\Middleware;
 
 use Closure;
+use App;
 
 class QaAuth
 {
     // Asks password for qa.* or alt.*
     public function handle($request, Closure $next)
     {
-        if (!$request->wantsJson()) {
-            $host = $request->getHost();
-            if (starts_with($host, 'qa.') || starts_with($host, 'alt.')) {
-                $name = config('app.name');
-                if (empty($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] != $name || $_SERVER['PHP_AUTH_PW'] != $name) {
-                    header('WWW-Authenticate: Basic realm="'.$name.'"');
-                    header('HTTP/1.0 401 Unauthorized');
-                    echo '401 Unauthorized';
-                    exit;
-                }
+        if (App::environment('staging') && !$request->wantsJson()) {
+            $name = config('app.name');
+            if (empty($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] != $name || $_SERVER['PHP_AUTH_PW'] != $name) {
+                header('WWW-Authenticate: Basic realm="'.$name.'"');
+                header('HTTP/1.0 401 Unauthorized');
+                echo '401 Unauthorized';
+                exit;
             }
         }
         
