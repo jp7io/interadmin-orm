851807bc6c2fd88ff65a005d97c9be8ef3380b04
JP Barbosa <jp@jp7.com.br>
2015-08-14 16:26:08 -0300

    Remove syntax highlight on SQL when debugging

--- Jp7/Debugger.class.php
+++ Jp7/Debugger.class.php
@@ -164,15 +164,7 @@ class Jp7_Debugger
 
     public function syntaxHighlightSql($sql, $style = '')
     {
-        if (!defined('PARSER_LIB_ROOT')) {
-            define('PARSER_LIB_ROOT', ROOT_PATH.'/inc/3thparty/sqlparserlib/');
-            echo '<style>';
-            readfile(PARSER_LIB_ROOT.'sqlsyntax.css');
-            echo '</style>';
-        }
-        require_once PARSER_LIB_ROOT.'sqlparser.lib.php';
-
-        return '<div class="debug_sql" style="'.$style.'">'.PMA_SQP_formatHtml(PMA_SQP_parse($sql)).'</div>';
+        return '<div class="debug_sql" style="'.$style.'">'.$sql.'</div>';
     }
 
     /**
