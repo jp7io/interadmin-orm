ccae46804084d82158887065226c2ff7e1eb2864
Rafael Testa <rafael@jp7.com.br>
2015-08-18 18:08:52 -0300

    Add tests to storage without resize and legacy template

--- tests/phpunit/interadmin/UploadTest.php
+++ tests/phpunit/interadmin/UploadTest.php
@@ -8,8 +8,9 @@ class UploadTest extends \PHPUnit_Framework_TestCase
     {
         global $config;
         $config = (object) [ 
-            'server' => (object)['host' => 'www.client.com.br'],
-            'name_id' => 'client'
+            'server' => (object)['host' => $this->appUrl()],
+            'name_id' => 'client',
+            'imagecache' => null,
         ];
     }
 
@@ -18,13 +19,16 @@ class UploadTest extends \PHPUnit_Framework_TestCase
 
     }
 
-     /**
+    /**
      * @dataProvider storageProvider
+     * @group fail
      */
     public function testUrlstorage($filePath, $expected, $template = null)
     {
         global $config;
-        $config = (object) ['storage' => ['host' => 'client.fakeurl.com'] ];
+        $config = (object) [
+        'storage' => ['host' => $this->storageUrl()],
+        ];
 
         $url = $this->url($filePath, $template);
 
@@ -34,9 +38,41 @@ class UploadTest extends \PHPUnit_Framework_TestCase
     public function storageProvider()
     {
         return [
+            ['../../upload/mediabox/00202630.jpeg', $this->storageUrl().'/upload/mediabox/00202630.jpeg'],
+            ['../../upload/mediabox/00202630.jpeg', $this->storageUrl().'/upload/mediabox/00202630.jpeg?size=40x40', 'thumb_interadmin'],
+            ['../../upload/mediabox/00202630.jpeg?v=2', $this->storageUrl().'/upload/mediabox/00202630.jpeg?v=2&size=40x40', 'thumb_interadmin'],
+            ['../../upload/mediabox/00202630.pdf', $this->storageUrl().'/upload/mediabox/00202630.pdf'],
+            ['../../upload/mediabox/00202630.pdf?v=2', $this->storageUrl().'/upload/mediabox/00202630.pdf?v=2'],
+            ['_default/file.css', '_default/file.css'],
+            [$this->externalUrl().'/upload/image.jpg', $this->externalUrl().'/upload/image.jpg'],
+            [$this->externalUrl().'/upload/image.jpg', $this->externalUrl().'/upload/image.jpg', 'thumb_interadmin']
+        ];
+    }
+
+     /**
+     * @dataProvider storageWithImageCacheProvider
+     */
+    public function testUrlstorageWithImageCache($filePath, $expected, $template = null)
+    {
+        global $config;
+        $config = (object) [
+        'storage' => ['host' => $this->storageUrl()],
+        'imagecache' => true, 
+        ];
+
+        $url = $this->url($filePath, $template);
+
+        $this->assertEquals($expected, $url);
+    }  
+
+    public function storageWithImageCacheProvider()
+    {
+        return [
             ['../../upload/mediabox/00202630.jpeg', $this->storageUrl().'/imagecache/original/mediabox/00202630.jpeg'],
             ['../../upload/mediabox/00202630.jpeg', $this->storageUrl().'/imagecache/thumb_interadmin/mediabox/00202630.jpeg', 'thumb_interadmin'],
+            ['../../upload/mediabox/00202630.jpeg?v=2', $this->storageUrl().'/imagecache/thumb_interadmin/mediabox/00202630.jpeg?v=2', 'thumb_interadmin'],
             ['../../upload/mediabox/00202630.pdf', $this->storageUrl().'/upload/mediabox/00202630.pdf'],
+            ['../../upload/mediabox/00202630.pdf?v=2', $this->storageUrl().'/upload/mediabox/00202630.pdf?v=2'],
             ['_default/file.css', '_default/file.css'],
             [$this->externalUrl().'/upload/image.jpg', $this->externalUrl().'/upload/image.jpg'],
             [$this->externalUrl().'/upload/image.jpg', $this->externalUrl().'/upload/image.jpg', 'thumb_interadmin']
@@ -56,9 +92,11 @@ class UploadTest extends \PHPUnit_Framework_TestCase
     public function LegacyProvider()
     {
         return [
-            ['../../upload/mediabox/00202630.jpeg', $this->appUrl().'/client/imagecache/original/mediabox/00202630.jpeg'],
-            ['../../upload/mediabox/00202630.jpeg', $this->appUrl().'/client/imagecache/thumb_interadmin/mediabox/00202630.jpeg', 'thumb_interadmin'],
+            ['../../upload/mediabox/00202630.jpeg', $this->appUrl().'/client/upload/mediabox/00202630.jpeg'],
+            ['../../upload/mediabox/00202630.jpeg', $this->appUrl().'/client/upload/mediabox/00202630.jpeg?size=40x40', 'thumb_interadmin'],
+            ['../../upload/mediabox/00202630.jpeg?v=2', $this->appUrl().'/client/upload/mediabox/00202630.jpeg?v=2&size=40x40', 'thumb_interadmin'],
             ['../../upload/mediabox/00202630.pdf', $this->appUrl().'/client/upload/mediabox/00202630.pdf'],
+            ['../../upload/mediabox/00202630.pdf?v=2', $this->appUrl().'/client/upload/mediabox/00202630.pdf?v=2'],
             ['_default/file.css', '_default/file.css'],
             [$this->externalUrl().'/upload/image.jpg', $this->externalUrl().'/upload/image.jpg'],
             [$this->externalUrl().'/upload/image.jpg', $this->externalUrl().'/upload/image.jpg', 'thumb_interadmin']
