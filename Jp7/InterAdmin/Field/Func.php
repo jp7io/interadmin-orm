82a407e0be7e5aea4b59de8a790be588012a8103
Carlos Rodrigues <carlos@jp7.com.br>
2016-02-17 14:02:58 -0200

    Recreate InterAdmin list

--- Jp7/InterAdmin/Field/Func.php
+++ Jp7/InterAdmin/Field/Func.php
@@ -4,18 +4,25 @@ class Jp7_InterAdmin_Field_Func extends Jp7_InterAdmin_Field_Base {
     public function getHeaderValue()
     {
         if (is_callable($this->nome)) {
-            return call_user_func($this->nome, (array) $this, '', 'header');
+            ob_start();
+            $response = call_user_func($this->nome, get_object_vars($this), '', 'header');
+            $response .= ob_get_clean();
+            return $response;
         } else {
-            return 'Função '.$this->nome.' não encontrada.';
+            return 'Function '.$this->nome.' not found.';
         }
     }
     
-    public function getListValue(ADOFetchObj $value)
+    public function getListValue(ADOFetchObj $record)
     {
         if (is_callable($this->nome)) {
-            return call_user_func($this->nome, (array) $this, $value, 'list');
+            $value = $this->getValue($record);
+            ob_start();
+            $response = call_user_func($this->nome, get_object_vars($this), $value, 'list');
+            $response .= ob_get_clean();
+            return $response;
         } else {
-            return 'Função '.$this->nome.' não encontrada.';
+            return 'Function '.$this->nome.' not found.';
         }
     }
 }
