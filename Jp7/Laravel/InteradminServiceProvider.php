75b03803efda10089163e535baa323df738537f0
Carlos Rodrigues <carlos@jp7.com.br>
2016-04-29 17:57:48 -0300

    Resolve Type class from current Route

--- Jp7/Laravel/InteradminServiceProvider.php
+++ Jp7/Laravel/InteradminServiceProvider.php
@@ -5,11 +5,13 @@ namespace Jp7\Laravel;
 use Illuminate\Support\ServiceProvider;
 use Jp7\Interadmin\DynamicLoader;
 use Jp7\Interadmin\Type;
+use Jp7\Laravel\RouterFacade as r;
 use Schema;
 use App;
 use PDOException;
 use Log;
 use View;
+use Route;
 
 /*
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -40,7 +42,12 @@ class InteradminServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        
+        App::singleton(Type::class, function () {
+            $route = Route::getCurrentRoute();
+            if ($route) { // Some pages don't have route, like 503.blade.php
+                return r::getTypeByRoute($route);
+            }
+        });
     }
     
     private function bootOrm()
