b58d53396b65b282deee1a13887ece928beff3ed
Carlos Rodrigues <carlos@jp7.com.br>
2016-05-02 17:38:26 -0300

    Create trait with common code for InterAdminController

---
+++ Jp7/Laravel/InteradminControllerTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Jp7\Laravel;
+
+use Illuminate\Http\Request;
+use Cookie;
+use Artisan;
+
+trait InteradminControllerTrait
+{
+    // Redirect to remote interadmin
+    public function index(Request $request)
+    {
+        return redirect('http://'.config('interadmin.host').'/'.config('app.name'));
+    }
+    
+    public function show(Request $request, $action)
+    {
+        if ($action === 'session') {
+            return $this->session($request);
+        }
+        if ($action === 'log-update') {
+            return $this->logUpdate();
+        }
+        return abort(404);
+    }
+    // Set cookie to flag that user MIGHT have access to interadmin
+    // Access should be validated elsewhere
+    public function session(Request $request)
+    {
+        $name = config('app.name');
+        $s_cookie = ['user' => $request->user];
+        // You can get it later with => Cookie::get('interadmin')
+        $cookie = Cookie::forever('interadmin', $s_cookie);
+        return response(date('c'))
+            ->withCookie($cookie)
+            ->withHeaders([
+                'Access-Control-Allow-Origin' => '*'
+            ]);
+    }
+    // Used to invalidate cache
+    public function logUpdate()
+    {
+        Artisan::call('httpcache:clear');
+        return date('c');
+    }
+}
