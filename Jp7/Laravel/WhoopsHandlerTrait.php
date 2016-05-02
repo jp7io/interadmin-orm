92dcad0527fda53cee6052def2bc9dd7c1279fd3
Carlos Rodrigues <carlos@jp7.com.br>
2016-05-02 17:50:58 -0300

    Add WhoopsHandlerTrait - Common code to use Whoops

---
+++ Jp7/Laravel/WhoopsHandlerTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Jp7\Laravel;
+
+use Exception;
+
+trait WhoopsHandlerTrait
+{
+    protected function convertExceptionToResponse(Exception $e)
+    {
+        if (config('app.debug')) {
+            return $this->renderExceptionWithWhoops($e);
+        }
+        return parent::convertExceptionToResponse($e);
+    }
+
+    /**
+     * Render an exception using Whoops.
+     *
+     * @param  \Exception $e
+     * @return \Illuminate\Http\Response
+     * @see https://mattstauffer.co/blog/bringing-whoops-back-to-laravel-5
+     */
+    protected function renderExceptionWithWhoops(Exception $e)
+    {
+        $whoops = new \Whoops\Run;
+        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+        return new \Illuminate\Http\Response(
+            $whoops->handleException($e),
+            $e->getStatusCode(),
+            $e->getHeaders()
+        );
+    }
+}
