be1412b4b036fe3875f672780004579f0738b53e
Carlos Rodrigues <carlos@jp7.com.br>
2016-05-12 15:07:49 -0300

    Fix security for QA and APP_DEBUG

--- Jp7/Laravel/InteradminServiceProvider.php
+++ Jp7/Laravel/InteradminServiceProvider.php
@@ -28,6 +28,13 @@ class InteradminServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Security: don't leak debug variables
+        if (config('app.debug') && !App::environment('local', 'testing')) {
+            if (gethostbyname('office.jp7.com.br') != \Request::ip()) {
+                \Config::set('app.debug', false);
+            }
+        }
+        
         BladeExtension::apply();
         
         $this->publishPackageFiles();
