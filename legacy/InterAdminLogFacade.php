172bb741ac442c4de0050564acf1088bace20ac7
Carlos Rodrigues <carlos@jp7.com.br>
2016-06-15 15:34:53 -0300

    Send Log::error() to Sentry

--- legacy/InterAdminLogFacade.php
+++ legacy/InterAdminLogFacade.php
@@ -15,5 +15,17 @@ class InterAdminLogFacade extends Illuminate\Support\Facades\Log
         $log = new \Illuminate\Log\Writer($monolog);		
         $log->useSyslog($config->name_id);		
         return $log;		
-    }		
+    }	
+    
+    public static function error($message, array $context = [])
+    {
+    	// Send Log::error() to Sentry
+        if ($message instanceof Exception && class_exists('Raven_Client')) {
+            $sentryClient = new Raven_Client(getenv('SENTRY_DSN'));
+            $sentryHandler = new Raven_ErrorHandler($sentryClient);
+            $sentryHandler->handleException($message);
+        }
+        
+        return parent::error($message, $context);
+    }
 }
