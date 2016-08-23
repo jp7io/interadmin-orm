290073e40cb3e153701bc0975069869cfb40db73
Carlos Rodrigues <carlos@jp7.com.br>
2016-08-23 10:43:22 -0300

    Fix Jp7_View_Helper_Box new InterAdmin without id_tipo

--- Jp7/View/Helper/Box.php
+++ Jp7/View/Helper/Box.php
@@ -9,8 +9,12 @@ class Jp7_View_Helper_Box extends Zend_View_Helper_Abstract
             throw new Exception('Unknown box: '.$id_box);
         }
 
+        $columnTipo = new Jp7_Model_BoxesTipo;
+        $boxIdTipo = $columnTipo->getInterAdminsChildren()['Boxes']['id_tipo'];
+
         $fakeRecord = new InterAdmin();
         $fakeRecord->id_box = $id_box;
+        $fakeRecord->id_tipo = $boxIdTipo;
         $fakeRecord->params = (object) $params;
 
         $box = new $classe($fakeRecord);
