6d1b1d0ecd3e8406e726f312214863cfd4d98fd0
Diego Garcia <12803744+Dugo03@users.noreply.github.com>
2018-10-04 16:51:42 -0300

    Fix find('id_slug') in tables without id_slug

--- Jp7/Interadmin/Query.php
+++ Jp7/Interadmin/Query.php
@@ -223,7 +223,7 @@ class Query extends Query\BaseQuery
         if (!$id) {
             return null; // save a query
         }
-        if (is_string($id) && !is_numeric($id)) {
+        if (is_string($id) && !is_numeric($id) && in_array('id_slug', $this->getModel()->getColumns())) {
             $this->options['where'][] = $this->_parseComparison('id_slug', '=', $id);
         } else {
             $this->options['where'][] = $this->_parseComparison('id', '=', $id);
